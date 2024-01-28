27ac359be81bd5250e42bc96e0073f0b86a67ccc
Maia <66437537+maia-s@users.noreply.github.com>
2024-01-28 21:44:19 +0100

    rename microproc-tests-macros to microproc-test-macros

--- microproc-tests/src/lib.rs
+++ microproc-tests/src/lib.rs
@@ -3,7 +3,7 @@ compile_error!("at least one of `proc-macro` or `proc-macro2` must be enabled");
 
 #[cfg(all(test, any(feature = "proc-macro", feature = "proc-macro2")))]
 mod tests {
-    use microproc_tests_macros::{literal_roundtrip, parse_rust_ops};
+    use microproc_test_macros::{literal_roundtrip, parse_rust_ops};
 
     macro_rules! test_literal {
         ($($lit:literal),*) => { $(
