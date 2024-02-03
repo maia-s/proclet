aacab0775d8a1365302dbb624112ea5f8ae6f436
Maia <66437537+maia-s@users.noreply.github.com>
2024-02-03 16:31:38 +0100

    clean up base.rs a little

--- microproc/src/base.rs
+++ microproc/src/base.rs
@@ -1,6 +1,10 @@
 use crate::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
 use std::fmt::Debug;
 
+// There are simpler and more contained ways to do this, but they fail type inference more.
+// For example, having the types in a fully bounded associated type instead of a base trait
+// fails type inference.
+
 macro_rules! impl_base {
     ($t:tt) => {
         impl_base!(@t1 $t $t);
@@ -8,7 +12,7 @@ macro_rules! impl_base {
         impl_base!(@i1 proc_macro2: "proc-macro2" $t $t);
     };
 
-    (@t1 {$($t:ident: $ts:ident $(: $tf:literal)?),* $(,)?} $t2:tt) => {
+    (@t1 {$($t:ident: $ts:ident),* $(,)?} $t2:tt) => {
         /// Base trait with associated type aliases for types from `proc-macro`/`proc-macro2`.
         /// See also [`ProcMacroExt`].
         pub trait ProcMacro: 'static + Clone + Debug {
@@ -21,37 +25,33 @@ macro_rules! impl_base {
         /// You can keep using the types defined in `ProcMacro`; the associated types of this trait
         /// should be considered an implementation detail.
         pub trait ProcMacroExt: ProcMacro<
-            $($t = Self::$ts,)*
+            $( $t = Self::$ts, )*
             TokenStreamIntoIter = Self::TokenStreamExtIntoIter
         > {
-            $( impl_base!(@t3 $t: $ts $(: $tf)?, $t2); )*
-
+            $( impl_base!(@t3 $t: $ts, $t2); )*
             type TokenStreamExtIntoIter: Clone + Iterator<Item = Self::TokenTreeExt>;
         }
     };
 
-    (@t2 $t:ident: $ts:ident, {$($t2:ident: $t2s:ident $(: $t2f:literal)?),* $(,)?}) => {
-        type $t: $t<$($t2 = Self::$t2,)*
+    (@t2 $t:ident: $ts:ident, {$($t2:ident: $t2s:ident),* $(,)?}) => {
+        type $t: $t<
+            $( $t2 = Self::$t2, )*
             TokenStreamIntoIter = Self::TokenStreamIntoIter,
         >;
     };
 
-    (@t3 $t:ident: $ts:ident $(: $tf:literal)?, {$($t2:ident: $t2s:ident $(: $t2f:literal)?),* $(,)?}) => {
-        $( #[cfg(feature = $tf)] )?
-        type $ts: crate::$ts<$($t2s = Self::$t2s,)*
+    (@t3 $t:ident: $ts:ident, {$($t2:ident: $t2s:ident),* $(,)?}) => {
+        type $ts: crate::$ts<
+            $( $t2s = Self::$t2s, )*
             TokenStreamExtIntoIter = Self::TokenStreamExtIntoIter,
         >;
-        $(
-            #[cfg(not(feature = $tf))]
-            type $ts;
-        )?
     };
 
-    (@i1 $pm:ident: $feature:literal {$($t:ident: $ts:ident $(: $tf:literal)?),* $(,)?} $t2:tt) => {
+    (@i1 $pm:ident: $feature:literal {$($t:ident: $ts:ident),* $(,)?} $t2:tt) => {
         $( impl_base!(@i2 $pm: $feature: $t, $t2); )*
     };
 
-    (@i2 $pm:ident: $feature:literal: $t:ident, {$($t2:ident: $t2s:ident $(: $t2f:literal)?),* $(,)?}) => {
+    (@i2 $pm:ident: $feature:literal: $t:ident, {$($t2:ident: $t2s:ident),* $(,)?}) => {
         #[cfg(feature = $feature)]
         impl ProcMacro for $pm::$t {
             $( type $t2 = $pm::$t2; )*
