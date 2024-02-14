71d0b5b782af121f37ab273c897e226491ce26c4
Maia <66437537+maia-s@users.noreply.github.com>
2024-02-14 23:34:12 +0100

    move proc macro main to closure

--- proclet-utils-macros/src/lib.rs
+++ proclet-utils-macros/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proclet::{
     delimited, op,
-    pm1::{Error, StringLiteral, TokenBuf},
+    pm1::{Error, StringLiteral},
     prelude::*,
     proclet,
 };
@@ -9,76 +9,75 @@ use std::collections::{hash_map::Entry, HashMap, HashSet};
 
 #[proc_macro]
 pub fn _define_ops(input: TokenStream) -> TokenStream {
-    proclet(input, _define_ops_)
-}
+    proclet(input, |input| {
+        let args = delimited(StringLiteral::parser(), op(",")).parse_all(input)?;
 
-fn _define_ops_(input: &mut &TokenBuf) -> Result<TokenStream, Error> {
-    let args = delimited(StringLiteral::parser(), op(",")).parse_all(input)?;
+        let mut map = HashMap::<String, (bool, HashSet<char>)>::new();
 
-    let mut map = HashMap::<String, (bool, HashSet<char>)>::new();
-
-    for (op, _) in args {
-        let str = op.value();
-        let clen = str.chars().count();
-        if clen == 0 {
-            return Err(Error::with_span(op.span(), "empty operator"));
-        } else if clen > 1 {
-            let mut chars = op.value().chars();
-            let mut ci = chars.next().unwrap().len_utf8();
-            for ch in chars {
-                let s = str[..ci].to_string();
-                ci += ch.len_utf8();
-                map.entry(s).or_insert((false, HashSet::new())).1.insert(ch);
-            }
-        }
-        match map.entry(op.into_value()) {
-            Entry::Occupied(e) => e.into_mut().0 = true,
-            Entry::Vacant(e) => {
-                e.insert((true, HashSet::new()));
-            }
-        }
-    }
-
-    let mut output = String::from(concat!(
-        "const fn __proclet_define_ops",
-        "(str: &::core::primitive::str, next: ::core::option::Option<::core::primitive::char>)",
-        "-> ::proclet::Match<::std::borrow::Cow<'static, ::core::primitive::str>>",
-        "{ use ::proclet::Match; use ::core::option::Option; use ::std::borrow::Cow;",
-        "match (str.as_bytes(), next) {"
-    ));
-    for (str, (valid, follow)) in map {
-        let bss = to_byte_string_string(&str);
-        if !follow.is_empty() {
-            output.push('(');
-            output.push_str(&bss);
-            output.push_str(", Option::Some(");
-            let mut it = follow.into_iter().peekable();
-            while let Some(ch) = it.next() {
-                output.push_str(&format!("\'\\u{{{:x}}}\'", u32::from(ch)));
-                if it.peek().is_some() {
-                    output.push('|');
+        for (op, _) in args {
+            let str = op.value();
+            let clen = str.chars().count();
+            if clen == 0 {
+                return Err(Error::with_span(op.span(), "empty operator"));
+            } else if clen > 1 {
+                let mut chars = op.value().chars();
+                let mut ci = chars.next().unwrap().len_utf8();
+                for ch in chars {
+                    let s = str[..ci].to_string();
+                    ci += ch.len_utf8();
+                    map.entry(s).or_insert((false, HashSet::new())).1.insert(ch);
                 }
             }
-            output.push_str(")) => ");
-            if valid {
-                output.push_str(&format!("Match::Partial(Cow::Borrowed({:?})),", &str));
-            } else {
-                output.push_str("Match::NeedMore,")
+            match map.entry(op.into_value()) {
+                Entry::Occupied(e) => e.into_mut().0 = true,
+                Entry::Vacant(e) => {
+                    e.insert((true, HashSet::new()));
+                }
             }
         }
-        if valid {
-            output.push('(');
-            output.push_str(&bss);
-            output.push_str(&format!(
-                ", _) => Match::Complete(Cow::Borrowed({:?})),",
-                &str
-            ));
+
+        let mut output = String::from(concat!(
+            "const fn __proclet_define_ops",
+            "(str: &::core::primitive::str, next: ::core::option::Option<::core::primitive::char>)",
+            "-> ::proclet::Match<::std::borrow::Cow<'static, ::core::primitive::str>>",
+            "{ use ::proclet::Match; use ::core::option::Option; use ::std::borrow::Cow;",
+            "match (str.as_bytes(), next) {"
+        ));
+        for (str, (valid, follow)) in map {
+            let bss = to_byte_string_string(&str);
+            if !follow.is_empty() {
+                output.push('(');
+                output.push_str(&bss);
+                output.push_str(", Option::Some(");
+                let mut it = follow.into_iter().peekable();
+                while let Some(ch) = it.next() {
+                    output.push_str(&format!("\'\\u{{{:x}}}\'", u32::from(ch)));
+                    if it.peek().is_some() {
+                        output.push('|');
+                    }
+                }
+                output.push_str(")) => ");
+                if valid {
+                    output.push_str(&format!("Match::Partial(Cow::Borrowed({:?})),", &str));
+                } else {
+                    output.push_str("Match::NeedMore,")
+                }
+            }
+            if valid {
+                output.push('(');
+                output.push_str(&bss);
+                output.push_str(&format!(
+                    ", _) => Match::Complete(Cow::Borrowed({:?})),",
+                    &str
+                ));
+            }
         }
-    }
-    output.push_str("_ => Match::NoMatch }}");
-    Ok(output
-        .parse()
-        .expect("internal error: generated invalid code"))
+        output.push_str("_ => Match::NoMatch }}");
+        let output: TokenStream = output
+            .parse()
+            .expect("internal error: generated invalid code");
+        Ok(output)
+    })
 }
 
 fn to_byte_string_string(str: &str) -> String {
--- str-block/src/lib.rs
+++ str-block/src/lib.rs
@@ -1,10 +1,5 @@
 use proc_macro::TokenStream;
-use proclet::{
-    delimited, op,
-    pm1::{Error, StringLiteral, TokenBuf},
-    prelude::*,
-    proclet, Optional,
-};
+use proclet::{delimited, op, pm1::StringLiteral, prelude::*, proclet, Optional};
 
 /// Remove the first line if it's empty except for whitespace, and remove the common whitespace prefix from
 /// all lines, if any. Empty lines are treated as if they have the common prefix.
@@ -12,61 +7,59 @@ use proclet::{
 /// Call with `{}` like `str_block!{"string"}` to stop rustfmt from modifying your string.
 #[proc_macro]
 pub fn str_block(input: TokenStream) -> TokenStream {
-    proclet(input, str_block_)
-}
+    proclet(input, |input| {
+        let strings = delimited(StringLiteral::parser(), Optional(op(","))).parse_all(input)?;
 
-fn str_block_(input: &mut &TokenBuf) -> Result<StringLiteral, Error> {
-    let strings = delimited(StringLiteral::parser(), Optional(op(","))).parse_all(input)?;
+        // concat input
+        let str = strings.into_iter().fold(String::new(), |mut s, item| {
+            s.push_str(item.0.value());
+            s
+        });
 
-    // concat input
-    let str = strings.into_iter().fold(String::new(), |mut s, item| {
-        s.push_str(item.0.value());
-        s
-    });
-
-    let mut lines = str.lines();
-    let mut lines2 = lines.clone();
-    let Some(first) = lines.next() else {
-        // input was an empty string
-        return Ok(StringLiteral::new(String::new()));
-    };
-    // skip first line if it's empty
-    let first = if first.trim().is_empty() {
-        lines2 = lines.clone();
-        if let Some(second) = lines.next() {
-            second
-        } else {
-            // input was one line of only whitespace
+        let mut lines = str.lines();
+        let mut lines2 = lines.clone();
+        let Some(first) = lines.next() else {
+            // input was an empty string
             return Ok(StringLiteral::new(String::new()));
-        }
-    } else {
-        first
-    };
+        };
+        // skip first line if it's empty
+        let first = if first.trim().is_empty() {
+            let _ = lines2.next();
+            if let Some(second) = lines.next() {
+                second
+            } else {
+                // input was one line of only whitespace
+                return Ok(StringLiteral::new(String::new()));
+            }
+        } else {
+            first
+        };
 
-    let first_trimmed = first.trim_start();
-    let mut prefix = &first[..first.len() - first_trimmed.len()];
-    if !prefix.is_empty() {
-        for line in lines {
-            if !line.is_empty() {
-                let ci = prefix
-                    .chars()
-                    .zip(line.chars())
-                    .take_while(|(p, l)| p == l)
-                    .fold(0, |ci, (p, _)| ci + p.len_utf8());
-                if ci < prefix.len() {
-                    prefix = &prefix[..ci];
-                    if prefix.is_empty() {
-                        break;
+        let first_trimmed = first.trim_start();
+        let mut prefix = &first[..first.len() - first_trimmed.len()];
+        if !prefix.is_empty() {
+            for line in lines {
+                if !line.is_empty() {
+                    let ci = prefix
+                        .chars()
+                        .zip(line.chars())
+                        .take_while(|(p, l)| p == l)
+                        .fold(0, |ci, (p, _)| ci + p.len_utf8());
+                    if ci < prefix.len() {
+                        prefix = &prefix[..ci];
+                        if prefix.is_empty() {
+                            break;
+                        }
                     }
                 }
             }
         }
-    }
 
-    let mut output = String::from(lines2.next().unwrap().strip_prefix(prefix).unwrap_or(""));
-    for line in lines2 {
-        output.push('\n');
-        output.push_str(line.strip_prefix(prefix).unwrap_or(""));
-    }
-    Ok(StringLiteral::new(output))
+        let mut output = String::from(lines2.next().unwrap().strip_prefix(prefix).unwrap_or(""));
+        for line in lines2 {
+            output.push('\n');
+            output.push_str(line.strip_prefix(prefix).unwrap_or(""));
+        }
+        Ok(StringLiteral::new(output))
+    })
 }
